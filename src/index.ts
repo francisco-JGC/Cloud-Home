883a3d4a95d1e0c40a748c010db6722f06f3c01b
Francisco Garcia <jjjchico1@gmail.com>
2023-01-18 14:30:23 -0600

    importing the index route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
@@ -8,6 +9,8 @@ app.set('port', process.env.PORT || 3000)
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', routes)
 
 app.listen(app.get('port'), () => {
     console.log(`\nListening on http://localhost:${app.get('port')}`)
