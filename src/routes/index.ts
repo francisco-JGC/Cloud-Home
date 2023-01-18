1c3c61d3127b68bc02f4c456b868b68bd3cc5044
Francisco Garcia <jjjchico1@gmail.com>
2023-01-18 14:26:19 -0600

    export router file dinamic

---
+++ src/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import fs from 'fs'
+
+const router = Router()
+const routerPath = __dirname
+
+const deleteFileExtension = (fileName: string): string => fileName.split('.')[0]
+
+fs.readdirSync(routerPath).forEach((file) => {
+    const skipFiles = ['index.ts', 'index.js']
+    if (!skipFiles.includes(file)) {
+        const fileName = deleteFileExtension(file)
+        const route = require(`./${ file}`)
+        router.use(`/${ fileName }`, route.default)
+
+        console.log(route.default)
+    }
+})
+
+export default router
