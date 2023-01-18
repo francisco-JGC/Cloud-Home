e614e676e4a269e32acc2adac4dcfb0868a836bb
Francisco Garcia <jjjchico1@gmail.com>
2023-01-18 17:19:09 -0600

    function to create recursively derectory and add the upload file to path

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -3,18 +3,39 @@ import multer from 'multer'
 import fs from 'fs'
 import { Request } from 'express'
 
-const storage = multer.diskStorage({
-    destination: (req: Request, file: Express.Multer.File , cb: (error: Error | null, destination: string) => void) => {
+const createDirectoryRecursively = (path: string): string => {
+    const pathArray = path.split('/')
+    let currentPath = ''
 
-        const dirPath: string = process.env.UPLOAD_DIR_PATH || '../uploads'
-        
+    pathArray.forEach((path) => {
+        currentPath += path + '/'
+        if (!fs.existsSync(currentPath)) {
+            fs.mkdirSync(currentPath)
+        }
+    })
+
+    return currentPath
+}
+
+const storage = multer.diskStorage({
+
+    destination: (req: Request, file: Express.Multer.File , cb: (error: Error | null, destination: string) => void) => {
+        const dirPath = 'uploads'
+        const directoryPath = req.query.directoryPath
+
         if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath)
         }
+
+        if (directoryPath) {
+            createDirectoryRecursively(`${dirPath}/${directoryPath}`)
+            return cb(null, `${dirPath}/${directoryPath}`)
+        }
+
         cb(null, dirPath)
     },
     filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-        cb(null, file.originalname)
+        cb(null, `${Date.now()}-${file.originalname}`)
     }
 })
 
