38639d683fb80b7f4b9081ebae63ff4033c5e563
Francisco Garcia <jjjchico1@gmail.com>
2023-01-18 13:06:17 -0600

    config express urlencoded

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(app.get('port'), () => {
     console.log(`\nListening on http://localhost:${app.get('port')}`)
